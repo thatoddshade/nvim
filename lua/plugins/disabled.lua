2212ea19650ed56090c872f69b71ccd1f7b94511
ðatoddshæd <thatoddshade.contact@skiff.com>
2024-03-20 14:41:07 +0100

    fix: disable `catpuccin/nvim` plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
 	{ "nvimdev/dashboard-nvim", enabled = false },
 	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
+	{ "catpuccin/nvim", enabled = false },
 }
