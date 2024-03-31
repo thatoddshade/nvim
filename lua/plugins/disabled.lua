476cd94dafba88641800cf74e4aa518aee934ba4
ðatoddshæd <thatoddshade.contact@skiff.com>
2024-03-31 19:26:03 +0200

    remove `lua/plugins/disabled.lua`

--- lua/plugins/disabled.lua
+++
@@ -1,7 +0,0 @@
-return {
-	{ "folke/noice.nvim", enabled = false },
-	{ "nvimdev/dashboard-nvim", enabled = false },
-	{ "nvim-neo-tree/neo-tree.nvim", enabled = false },
-	{ "folke/persistence.nvim", enabled = false },
-	{ "catpuccin/nvim", enabled = false },
-}
