a6d2ce6a8281b0b7e04084a30a7fdf47dfdff9ae
ðatoddshæd <thatoddshade.contact@skiff.com>
2024-04-21 21:44:16 +0200

    feat: set `vim.g.localleader` to " "
    stolen from https://github.com/nvim-lua/kickstart.nvim/blob/2e68a2c2532d1575b717721a34efa033f4d7a88e/init.lua#L91C1-L91C27

--- init.lua
+++ init.lua
@@ -2,6 +2,7 @@ vim.cmd[[set mousemodel=extend]]
 vim.cmd[[set clipboard+=unnamedplus]]
 vim.opt.termguicolors = true
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
