12a52c7a462e787863038818fd93f380fafdace0
thatoddshade <thatoddshade.contact@skiff.com>
2024-05-10 14:45:50 +0200

    style: `lua/plugins/lsp.lua` edit

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -47,7 +47,9 @@ return {
 							Lua = {
 					runtime = { version = "Lua 5.1" },
 								diagnostics = {
-									globals = { "vim", "it", "describe", "before_each", "after_each" },
+									globals = {
+                                        "vim", "it", "describe", "before_each", "after_each"
+                                    },
 								}
 							}
 						}
